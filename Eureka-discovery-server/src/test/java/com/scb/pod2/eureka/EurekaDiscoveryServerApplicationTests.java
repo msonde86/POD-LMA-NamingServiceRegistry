8c1121aa890274f4ef85e97201e18f1a396d5a55
unknown <ctsjavauser298@iiht.tech>
2020-09-02 17:16:06 +0530

    sprint 2- Eureka server commit

---
+++ Eureka-discovery-server/src/test/java/com/scb/pod2/eureka/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.pod2.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
