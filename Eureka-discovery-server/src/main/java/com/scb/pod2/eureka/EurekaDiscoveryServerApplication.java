dfec69b11379f41056465655027d56ac18760922
ctsjavauser281 <ctsjavauser281@ctsjavafs122.iiht.tech>
2020-09-16 10:24:48 +0530

    Sonar scan changes

--- Eureka-discovery-server/src/main/java/com/scb/pod2/eureka/EurekaDiscoveryServerApplication.java
+++ Eureka-discovery-server/src/main/java/com/scb/pod2/eureka/EurekaDiscoveryServerApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import lombok.Generated;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaDiscoveryServerApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
 	}
